ed62129235ec5d099f694c438c4e46b90ea51bd8
patrick <pdeyoreo@gmail.com>
2019-01-19 01:49:44 +0100

    Added 'shroom.html' page Added javascript behavior for 'shroom.html'

---
+++ html/behavior.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var thumbnailElement = document.getElementById("smart_thumbnail");
+
+  thumbnailElement.addEventListener("click", function() {
+    if (thumbnailElement.className == "small") {
+      thumbnailElement.className = "";
+    } else {
+      thumbnailElement.className = "small";
+    }
+  });
+
+});
